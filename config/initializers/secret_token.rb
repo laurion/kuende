518cd88735c79ea16a3104052534e61c99121473
Teodor Pripoae <teodor.pripoae@gmail.com>
2013-03-22 17:20:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kuende::Application.config.secret_key_base = 'd44d48107f0908b02803d5eb37b924256f5bda4802d2a7fcd5c336f46c8d8d11c2069c4c998a39553ef4cb609faa107108a5c618671b9d1fdf7c5dc9c6b178e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kuende::Application.config.session_store :encrypted_cookie_store, key: '_kuende_session'
